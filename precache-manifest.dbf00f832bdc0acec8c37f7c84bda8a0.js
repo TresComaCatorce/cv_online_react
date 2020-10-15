ee3a2878c23c4245d16d91d8dd01bef6cd9e04d5
Ferrero Cristian <cristian.ferrero1992@gmail.com>
2020-10-15 16:59:41 -0300

    Change files to build

---
+++ precache-manifest.dbf00f832bdc0acec8c37f7c84bda8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce98530052e877e794f17878cc0b3d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66268caac81008e40a28",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "80a68a8d6b5445038f5f",
+    "url": "/static/css/main.72c95166.chunk.css"
+  },
+  {
+    "revision": "66268caac81008e40a28",
+    "url": "/static/js/2.7f49c80a.chunk.js"
+  },
+  {
+    "revision": "8331206ce23e24a93191c33442867afc",
+    "url": "/static/js/2.7f49c80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a68a8d6b5445038f5f",
+    "url": "/static/js/main.86556abf.chunk.js"
+  },
+  {
+    "revision": "7404282943dd4b8ce915",
+    "url": "/static/js/runtime-main.95b85ecb.js"
+  }
+]);
